77e8303e6ca8dd3407c4754c9fc5cdde7424493f
Evtimov <azsumkiro@gmail.com>
2020-02-01 15:58:39 +0200

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    persons: [
+       { name: 'Evtim', age: 21},
+       { name: 'Burov', age: 20}
+    ],
+    otherState: 'test',
+    showPersons: false
+  };
 
+   switchNameHandler = () => {
+     this.setState({
+     persons: [
+     { name: 'Evto', age: 27},
+     { name: 'Burov1', age: 20}
+     ]
+    });
+   };
+
+   nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+      { name: 'Evtim', age: 21},
+      { name: event.target.value, age: 20}
+      ]
+     });
+   }
+
+   togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+   }
+   render() {
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+<div>  
+  {this.state.persons.map(person => {
+    return <Person 
+    name={person.name} 
+    age={person.age} />
+  })}
+   </div> 
+      );
+    }
+
+    return (
+      <div className="App">
+      <h1>Hi, welcome to my website!</h1>
+      <Button variant="outlined" onClick={this.togglePersonsHandler}>Show Info</Button>
+      {persons}
+       
+   </div>
+   ); 
+     
+  //  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now ?'));
+  }
+ }
+ 
 export default App;
+
